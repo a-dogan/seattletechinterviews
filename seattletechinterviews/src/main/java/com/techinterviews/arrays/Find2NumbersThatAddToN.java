a42e45ae9f0b33627d2b4cf1579c101fce1e1c8b
adogan <artdogan@gmail.com>
2014-01-05 14:44:10 -0800

    Passes all but big numbers test

--- seattletechinterviews/src/main/java/com/techinterviews/arrays/Find2NumbersThatAddToN.java
+++ seattletechinterviews/src/main/java/com/techinterviews/arrays/Find2NumbersThatAddToN.java
@@ -1,5 +1,8 @@
 package com.techinterviews.arrays;
 
+import java.util.HashSet;
+import java.util.ListIterator;
+
 /**
  * Find 2 numbers in an array that add up to a given number.
  * 
@@ -10,6 +13,27 @@ public class Find2NumbersThatAddToN {
 	
 	public int[] algorithm(int array[],int n){
 		int result[]={};
-		return null;
+		HashSet<Integer> intSet = new HashSet<Integer>();
+		
+		if(array==null || 0==array.length || n>Integer.MAX_VALUE)
+			return result;
+		
+		int valToCheck;
+		//add and check the difference in the hashset before adding
+		for(int i=0;i<array.length;i++){
+			if(array[i]<Integer.MAX_VALUE){
+				valToCheck=array[i];
+				int diff=n-valToCheck;
+				if(intSet.contains(diff)){
+					result=new int[2];
+					result[0]=diff;
+					result[1]=valToCheck;
+					return result;
+				}
+				else	
+					intSet.add(valToCheck);	
+			}	
+		}
+		return result;
 	}
 }
