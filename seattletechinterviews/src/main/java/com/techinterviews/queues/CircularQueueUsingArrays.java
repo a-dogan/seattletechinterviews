d970f6ec1a3e37676973979f255c998afb15a513
unknown <artdog@SEA-9901281719.ant.amazon.com>
2014-01-10 16:21:42 -0800

    circular queue

--- seattletechinterviews/src/main/java/com/techinterviews/queues/CircularQueueUsingArrays.java
+++ seattletechinterviews/src/main/java/com/techinterviews/queues/CircularQueueUsingArrays.java
@@ -2,16 +2,43 @@ package com.techinterviews.queues;
 
 public class CircularQueueUsingArrays {
 	int [] array;
+	int head=0;
+	int tail=0;
 	
 	public CircularQueueUsingArrays(int size){
+
+		if(size>0)
+			array = new int[size];
+	}
+	
+	private boolean isFilled(){
+		
+		if(tail==(array.length-1))
+				return true;
+		
+		return false;
 	}
 	
 	public void enqueue(int v){
+		
+		//if filled add to head
+		if(isFilled()){
+			array[head]=v;
+			head++;
+		}
+		else{
+			array[tail]=v;
+			tail++;
+		}
 	}
 	
 	public int dequeue(){
-		return 0;
-	}
-	
+		int v = 0;
+		if(head!=0 && tail!=0){
+			v = this.array[head];
+			tail--;
+		}
 
+		return v;
+	}
 }
